f77c7055e91390121ee3c4748042fb184632ce70
niposch <niposch@gmail.com>
2022-10-19 18:33:34 +0200

    Connected api to frontend

--- backend/STExS/Program.cs
+++ backend/STExS/Program.cs
@@ -14,6 +14,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 }
 
 app.UseHttpsRedirection();
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'STExS';
+
+  public temperatures:Array<number> = [];
+  constructor(private readonly http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Array<TemperatureResponse>>('api/weatherforecast').subscribe((response) => {
+      this.temperatures = response.map((temperatureResponse) => temperatureResponse.temperatureC);
+      console.table(response);
+    });
+  }
+}
+
+interface TemperatureResponse {
+  date:Date,
+  temperatureC:number,
+  temperatureF:number,
+  summary:string
 }
