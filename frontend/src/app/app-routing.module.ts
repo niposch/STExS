6c0166d1c603b23c627ab3d2bde48f91820a5640
TarantelFaser <jonathan@boeckel.name>
2022-11-15 18:02:47 +0100

    fixed routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/authentication/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"auth/login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
