0e72285c07529deef1ab635ff7aa50e98a86f032
niposch <niposch@gmail.com>
2022-11-26 11:16:52 +0100

    Refactorings

--- Parson/Parson/Program.cs
+++ Parson/Parson/Program.cs
@@ -1,53 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Parson
 {
-    class Stringular //: BaseExercise
+    internal class ParsonElement //: BaseExercise
     {
-        
-        private String snippet;
-        private int index;
-
-        public String Snippet{
-            get{return snippet;}
-        }
-        public int Index{
-            get{return index;}
-}
-        public Stringular(String code, int index) {
-            this.snippet=code;
-            this.index=index;
-        }
-
-
-     }
-
-    class Parson //:BaseExercise
+        public ParsonElement(string code,
+            int index)
         {
-        String originalCode;
-        private List<Stringular> puzzle;
-
-        public Parson(String input){
-            this.originalCode=input;
-            this.puzzle=new List<Stringular>();
-            this.blocksize();
+            Snippet = code;
+            Index = index;
         }
 
-        private int countLines() {
-            int linecount=0;
-            for (int i = 0; i < originalCode.Length; i++) {
+        public string Snippet { get; }
+
+        public int Index { get; }
+    }
+
+    internal class Parson //:BaseExercise
+    {
+        private readonly string originalCode;
+        private readonly List<ParsonElement> puzzle;
+
+        public Parson(string input)
+        {
+            originalCode = input;
+            puzzle = new List<ParsonElement>();
+            Blocksize();
+        }
+
+        private int CountLines()
+        {
+            var linecount = 0;
+            for (var i = 0; i < originalCode.Length; i++)
                 if (originalCode[i] == '\n')
                     linecount++;
-            }
             return linecount;
         }
 
-        private void blocksize() {
-            int lines = countLines();
+        private void Blocksize()
+        {
+            var lines = CountLines();
             int blocksize;
             if (lines <= 4)
                 blocksize = 1;
@@ -58,78 +51,79 @@ namespace Parson
             else
                 blocksize = 5;
 
-            split(blocksize);
+            Split(blocksize);
         }
 
-        private void split(int blocksize) {
-            int counter = 0;
-            int currentBlock=0;
-            int startindex = 0;
-            for (int i = 0; i < this.originalCode.Length; i++) {
-                if (this.originalCode[i] == '\n') {
-                    counter++;
-                    if (counter == blocksize) {
-                        
-                        Stringular temp= new Stringular(this.originalCode.Substring(startindex, i - startindex), currentBlock);
-                        this.puzzle.Add(temp);
-                        counter = 0;
-                        startindex = i+1;
-                        currentBlock++;
-                        
-               
-                    }
-                   
-                }
-                 if(i==(this.originalCode.Length-1)){
-                        Stringular temp= new Stringular(this.originalCode.Substring(startindex), currentBlock);
-                        this.puzzle.Add(temp);
-                    }
-            }
-        }
-
-        public void shuffle(){
-         Random rng = new Random();
-         int n= this.puzzle.Count;
-            while (n>1){
-                n--;
-                int k = rng.Next(n+1);
-                Stringular temp= puzzle[k];
-                puzzle[k] =puzzle[n];
-                puzzle[n]=temp;
-            }
-        }
-
-        public int finalScore(int maxScore){
-            int correctBlocks=0;
-            for(int i=0; i<this.puzzle.Count; i++){
-            if(i==this.puzzle[i].Index)
-                    correctBlocks++;
-            }
-            float achievedPoints= (float)correctBlocks/this.puzzle.Count;
-            return (int) (maxScore*achievedPoints+0.5f);
-        }
-
-        public void showContent(){
-        for(int i=0; i<this.puzzle.Count; i++){
-                Console.WriteLine(this.puzzle[i].Index+" "+this.puzzle[i].Snippet );
-            }
-        }
-        /*
-        static void Main(string[] args)
+        private void Split(int blocksize)
         {
-            String input = "line1\nline2\nline3 \nline4\nline5 \nline6 \nline7";
-            Parson test = new Parson(input);
-            test.showContent();
-            test.shuffle();
-            Console.WriteLine();
-            test.showContent();
-            Console.WriteLine("\n Score:"+test.finalScore(10));
-             test.shuffle();
-            Console.WriteLine();
-            test.showContent();
-            Console.Read();
+            var counter = 0;
+            var currentBlock = 0;
+            var startindex = 0;
+            for (var i = 0; i < originalCode.Length; i++)
+            {
+                if (originalCode[i] == '\n')
+                {
+                    counter++;
+                    if (counter == blocksize)
+                    {
+                        var temp = new ParsonElement(originalCode.Substring(startindex, i - startindex), currentBlock);
+                        puzzle.Add(temp);
+                        counter = 0;
+                        startindex = i + 1;
+                        currentBlock++;
+                    }
+                }
 
+                if (i == originalCode.Length - 1)
+                {
+                    var temp = new ParsonElement(originalCode.Substring(startindex), currentBlock);
+                    puzzle.Add(temp);
+                }
+            }
         }
-        */
+
+        public void Shuffle()
+        {
+            var rng = new Random();
+            var n = puzzle.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                var temp = puzzle[k];
+                puzzle[k] = puzzle[n];
+                puzzle[n] = temp;
+            }
+        }
+
+        public int FinalScore(int maxScore)
+        {
+            var correctBlocks = 0;
+            for (var i = 0; i < puzzle.Count; i++)
+                if (i == puzzle[i].Index)
+                    correctBlocks++;
+            var achievedPoints = (float)correctBlocks / puzzle.Count;
+            return (int)(maxScore * achievedPoints + 0.5f);
+        }
+
+        public void ShowContent()
+        {
+            for (var i = 0; i < puzzle.Count; i++) Console.WriteLine(puzzle[i].Index + " " + puzzle[i].Snippet);
+        }
+
+        private static void Main(string[] args)
+        {
+            var input = "line1\nline2\nline3 \nline4\nline5 \nline6 \nline7";
+            var test = new Parson(input);
+            test.ShowContent();
+            test.Shuffle();
+            Console.WriteLine();
+            test.ShowContent();
+            Console.WriteLine("\n Score:" + test.FinalScore(10));
+            test.Shuffle();
+            Console.WriteLine();
+            test.ShowContent();
+            Console.Read();
+        }
     }
 }
