c2f2529a8f027daf7123cf56bf63b4861eedba85
niposch <niposch@gmail.com>
2022-12-10 02:06:12 +0100

    fixed fe tests

--- frontend/src/app/guards/auth.guard.spec.ts
+++ frontend/src/app/guards/auth.guard.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {AppComponent} from "../app.component";
 
 describe('AuthGuard', () => {
+
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    }).compileComponents();
     guard = TestBed.inject(AuthGuard);
   });
 
