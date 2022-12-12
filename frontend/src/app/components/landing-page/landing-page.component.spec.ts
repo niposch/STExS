518c35cb7007e97f0a4762f8fb0436069d0e8f40
TarantelFaser <jonathan@boeckel.name>
2022-12-12 15:35:20 +0100

    Implemented Testing module for Landing Page component

--- frontend/src/app/components/landing-page/landing-page.component.spec.ts
+++ frontend/src/app/components/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +10,8 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
+      declarations: [ LandingPageComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
