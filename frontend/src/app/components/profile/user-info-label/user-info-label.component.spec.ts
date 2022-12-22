d58cdbb5d9010d16afff2688d8c1b521d9c1fdc2
TarantelFaser <jonathan@boeckel.name>
2022-12-22 19:40:09 +0100

    Added test for UserInfoLabelComponent

--- frontend/src/app/components/profile/user-info-label/user-info-label.component.spec.ts
+++ frontend/src/app/components/profile/user-info-label/user-info-label.component.spec.ts
@@ -1,10 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import { UserInfoLabelComponent } from './user-info-label.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
-import {ProfileComponent} from "../profile.component";
-import {By} from "@angular/platform-browser";
 
 describe('UserInfoLabelComponent', () => {
   let component: UserInfoLabelComponent;
@@ -27,33 +24,24 @@ describe('UserInfoLabelComponent', () => {
   });
 
   it ('#editButtonClick() should toggle #isEditing', () => {
-    // @ts-ignore
-    const comp = new UserInfoLabelComponent();
-    expect(comp.isEditing)
+    expect(component.isEditing)
       .withContext('not editing at first')
       .toBe(false)
-    comp.editButtonClick();
-    expect(comp.isEditing)
+    component.editButtonClick();
+    expect(component.isEditing)
       .withContext('editing after click')
       .toBe(true);
-    comp.editButtonClick();
-    expect(comp.isEditing)
+    component.editButtonClick();
+    expect(component.isEditing)
       .withContext('not editing after second click')
       .toBe(false);
   });
 
-/*
-  // Figure out how to trigger Events
-  it ('#validateEmail() should validate "test@testdev.com" correctly', ()=>{
-    // @ts-ignore
-    const component = new UserInfoLabelComponent();
-
-    expect(component.showEmailError).toBe(true);
-    expect(component.emailIsCorrect).toBe(false);
-    component.validateEmail({target:{value:"test@testdev.com"}});
-    expect(component.showEmailError).toBe(false);
-    expect(component.emailIsCorrect).toBe(true);
-  });
-
- */
+  it ('edit button should activate edit mode', fakeAsync(() => {
+    spyOn(component, 'startEditing');
+    let button = fixture.debugElement.nativeElement.querySelectorAll('app-edit-button')[0];
+    button.click();
+    tick();
+    expect(component.startEditing).toHaveBeenCalled();
+  }));
 });
