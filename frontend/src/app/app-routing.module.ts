11fd473c91c8577e53d013d3f36be504494b5fe4
TarantelFaser <jonathan@boeckel.name>
2022-12-25 01:52:39 +0100

    Fixed Name input sizing on register form

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 import {HeaderOnlyLayoutComponent} from "./layout/header-only-layout/header-only-layout.component";
-import {NotfoundComponent} from "./components/notfound/notfound.component";
 
 const routes: Routes = [
   {
@@ -11,14 +10,7 @@ const routes: Routes = [
     children: [
       {path: "", component: LandingPageComponent},
     ]
-  },
-  {
-    path: '**',
-    component: HeaderOnlyLayoutComponent,
-    children: [
-      {path: "", component: NotfoundComponent},
-    ]
-  },
+  }
 ];
 
 @NgModule({
