d04fbd82f5790719c2dde46edcacc444f5dd545b
TarantelFaser <jonathan@boeckel.name>
2022-12-25 01:24:22 +0100

    Added 404 Not Found Page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 import {HeaderOnlyLayoutComponent} from "./layout/header-only-layout/header-only-layout.component";
+import {NotfoundComponent} from "./components/notfound/notfound.component";
 
 const routes: Routes = [
   {
@@ -10,7 +11,14 @@ const routes: Routes = [
     children: [
       {path: "", component: LandingPageComponent},
     ]
-  }
+  },
+  {
+    path: '**',
+    component: HeaderOnlyLayoutComponent,
+    children: [
+      {path: "", component: NotfoundComponent},
+    ]
+  },
 ];
 
 @NgModule({
