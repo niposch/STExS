134f25ced574bd85c457fe248c7033cacfdbd79e
niposch <niposch@gmail.com>
2022-12-28 13:11:13 +0100

    Created module administrate component

--- frontend/src/app/components/admin/modules-admin/administrate-module/administrate-module.component.ts
+++ frontend/src/app/components/admin/modules-admin/administrate-module/administrate-module.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ModuleService} from "../../../../../services/generated/services/module.service";
+import {ModuleDetailItem} from "../../../../../services/generated/models/module-detail-item";
 
 @Component({
   selector: 'app-administrate-module',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministrateModuleComponent implements OnInit {
 
-  constructor() { }
+  public module: ModuleDetailItem|null = null;
+  constructor(private readonly activatedRoute:ActivatedRoute,
+              private readonly moduleService: ModuleService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if(params["id"] != null){
+        this.loadModule(params["id"])
+      }
+    })
   }
 
+  loadModule(id:string){
+    this.moduleService.apiModuleGetByIdGet$Json({
+      id: id
+    })
+      .subscribe(m => this.module = m)
+  }
 }
