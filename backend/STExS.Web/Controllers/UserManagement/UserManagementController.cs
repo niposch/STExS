7e59b6c63fd8954e5f75174fe6a6f47f6a220682
niposch <niposch@gmail.com>
2023-01-10 17:21:31 +0100

    Anfang User Management Controller

---
+++ backend/STExS.Web/Controllers/UserManagement/UserManagementController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Helper.Roles;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace STExS.Controllers.UserManagement;
+
+[Authorize(Roles = $"{RoleHelper.Admin}")]
+public class UserManagementController
+{
+    [HttpGet("all")]
+    public async Task GetAllUsers()
+    {
+    }
+}
