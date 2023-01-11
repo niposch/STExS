4d4f41c4c90fe850276e843e159f437a0496887e
Levis <waxddxawd@gmail.com>
2023-01-11 19:26:03 +0100

    add userManagermentController

---
+++ backend/Application/DTOs/UserAdmin/UserListItem.cs
@@ -0,0 +1,16 @@
+﻿using Application.Helper.Roles;
+
+namespace Application.DTOs.UserAdmin;
+
+public class UserListItem
+{
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string MatrikelNummer { get; set; }
+    public string PhoneNumber { get; set; }
+    public RoleType CurrentRole { get; set; }
+    public Guid UserId { get; set; }
+    
+}
--- backend/STExS.Web/Controllers/UserManagement/UserManagementController.cs
+++ backend/STExS.Web/Controllers/UserManagement/UserManagementController.cs
@@ -1,14 +1,59 @@
-﻿using Application.Helper.Roles;
+﻿using Application.DTOs.UserAdmin;
+using Application.Helper.Roles;
+using Common.Models.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace STExS.Controllers.UserManagement;
+[ApiController]
+[Route("api/[controller]")]
 
-[Authorize(Roles = $"{RoleHelper.Admin}")]
-public class UserManagementController
+public class UserManagementController : ControllerBase
 {
-    [HttpGet("all")]
-    public async Task GetAllUsers()
+    private readonly UserManager<ApplicationUser> userManager;
+    private readonly RoleManager<ApplicationRole> roleManager;
+
+    public UserManagementController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
     {
+        this.userManager = userManager;
+        this.roleManager = roleManager;
     }
-}
+    [HttpGet("all")]
+    [Authorize(Roles = $"{RoleHelper.Admin}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<UserListItem>))]
+    
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var userListItems = new List<UserListItem>();
+        foreach (var user in this.userManager.Users)
+        {
+            var listItem = new UserListItem
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                MatrikelNummer = user.MatrikelNumber,
+                PhoneNumber = user.PhoneNumber,
+                UserId = user.Id,
+                UserName = user.UserName
+            };
+            listItem.CurrentRole = RoleType.User;
+            var roles = await this.userManager.GetRolesAsync(user);
+            if (roles.Contains(RoleHelper.Teacher))
+            {
+                listItem.CurrentRole = RoleType.Teacher;
+            }
+            if (roles.Contains(RoleHelper.Admin))
+            {
+                listItem.CurrentRole = RoleType.Admin;
+            }
+            userListItems.Add(listItem);
+        }
+
+
+        return this.Ok(userListItems);
+    }
+    
+}  
