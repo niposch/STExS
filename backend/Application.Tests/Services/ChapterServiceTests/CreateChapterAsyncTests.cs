54b7ffa67815de0a78aa9f237f1bfa82bf50d773
Mikhail <mikhail.b@gmx.de>
2023-01-13 01:17:07 +0100

    CreateChapterAsyncTest wip (UnauthorizedException)

---
+++ backend/Application.Tests/Services/ChapterServiceTests/CreateChapterAsyncTests.cs
@@ -0,0 +1,61 @@
+﻿using Application.Helper.Roles;
+using Common.Models.Authentication;
+using Common.Models.ExerciseSystem;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Application.Tests.Services.ChapterServiceTests;
+
+public sealed class CreateChapterAsyncTests : Infrastructure
+{
+    private string chapterName = null!;
+    private string chapterDescription = null!;
+    private Guid moduleId;
+    private Guid ownerId;
+
+    [Fact]
+
+    public async Task CreatesChapter()
+    {
+        // Arrange
+       this.chapterName = "Test Chapter";
+       this.chapterDescription = "Test Chapter Description";
+       this.moduleId = Guid.NewGuid();
+       this.ownerId = Guid.NewGuid();
+       
+       this.ApplicationDbContext.Modules.Add(this.Fixture.Build<Module>()
+           .With(m => m.Id, this.moduleId)
+           .With(m => m.OwnerId, this.ownerId)
+           .Create());
+       var user = this.Fixture.Build<ApplicationUser>()
+           .With(m => m.Id, this.ownerId)
+           .Create();
+       this.ApplicationDbContext.SaveChanges();
+       
+       this.UserManagerMock.Setup(m => m.FindByIdAsync(this.ownerId.ToString()))
+           .ReturnsAsync(user);
+        
+       this.UserManagerMock.Setup(m => m.GetRolesAsync(user))
+           .ReturnsAsync(new List<string>{RoleHelper.Admin});
+       
+       // Act
+       await this.CallAsync();
+       
+       // Assert
+       var chapter = await this.ApplicationDbContext.Chapters.FirstOrDefaultAsync();
+       chapter.Should().NotBeNull();
+       chapter!.ChapterName.Should().Be(this.chapterName);
+       chapter.ChapterDescription.Should().Be(this.chapterDescription);
+       chapter.ModuleId.Should().Be(this.moduleId);
+       chapter.OwnerId.Should().Be(this.ownerId);
+       
+    }
+
+    private Task CallAsync()
+    {
+        return this.ChapterService.CreateChapterAsync(this.moduleId,
+            this.chapterName,
+            this.chapterDescription, 
+            this.ownerId);
+    }
+}
---
+++ backend/Application.Tests/Services/ChapterServiceTests/Infrastructure.cs
@@ -0,0 +1,38 @@
+﻿using Application.Services;
+using Common.Models.Authentication;
+using Common.Models.ExerciseSystem;
+using Common.RepositoryInterfaces.Generic;
+using Common.RepositoryInterfaces.Tables;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Repositories;
+using Repositories.Repositories;
+using TestHelper;
+
+namespace Application.Tests.Services.ChapterServiceTests;
+
+public abstract class Infrastructure
+{
+    protected readonly IFixture Fixture;
+    protected readonly ApplicationDbContext ApplicationDbContext;
+    protected readonly ChapterService ChapterService;
+    protected readonly Mock<UserManager<ApplicationUser>> UserManagerMock;
+    protected readonly Mock<ModuleRepository> ModuleRepositoryMock;
+
+    protected Infrastructure()
+    {
+        this.Fixture = new Fixture().Customize(new AutoMoqCustomization());
+        this.Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        
+        this.UserManagerMock = this.Fixture.FreezeInject<UserManager<ApplicationUser>>();
+        this.ModuleRepositoryMock = this.Fixture.FreezeInject<ModuleRepository>();
+        
+        this.Fixture.Register<IChapterRepository>(this.Fixture.Create<ChapterRepository>);
+        
+        this.Fixture.Register<IApplicationRepository>(this.Fixture.Create<ApplicationRepository>);
+        
+        this.ApplicationDbContext = this.Fixture.InjectInMemoryDbContext<ApplicationDbContext>();
+        
+        this.ChapterService = this.Fixture.Create<ChapterService>();
+    }
+}
