1234d901c636d0425132768cc12705b23d6eb626
TarantelFaser <jonathan@boeckel.name>
2023-01-15 19:29:17 +0100

    Changes by niposch

--- frontend/src/app/components/module/delete-dialog/delete-dialog.component.spec.ts
+++ frontend/src/app/components/module/delete-dialog/delete-dialog.component.spec.ts
@@ -3,17 +3,21 @@ import {DeleteDialogComponent} from './delete-dialog.component';
 import {RouterTestingModule} from "@angular/router/testing";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {MatDialogRef} from "@angular/material/dialog";
+import {By} from "@angular/platform-browser";
 
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
+  let dialogRef = {
+    close: () => {}
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ DeleteDialogComponent ],
       imports: [ RouterTestingModule, HttpClientTestingModule],
       providers: [
-        { provide: MatDialogRef, useValue: {} }
+        { provide: MatDialogRef<DeleteDialogComponent>, useValue: {dialogRef} }
       ]
     })
     .compileComponents();
