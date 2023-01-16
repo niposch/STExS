20c3ed995b0eff80d960f1da236ed2b096290d44
niposch <niposch@gmail.com>
2023-01-16 01:55:14 +0100

    Fixed unit tests

--- frontend/src/app/components/admin/exercise-admin/create-exercise/create-edit-code-output/create-edit-code-output.component.spec.ts
+++ frontend/src/app/components/admin/exercise-admin/create-exercise/create-edit-code-output/create-edit-code-output.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateEditCodeOutputComponent } from './create-edit-code-output.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 describe('CreateEditCodeOutputComponent', () => {
   let component: CreateEditCodeOutputComponent;
@@ -8,7 +11,8 @@ describe('CreateEditCodeOutputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateEditCodeOutputComponent ]
+      declarations: [ CreateEditCodeOutputComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule, MatSnackBarModule]
     })
     .compileComponents();
 
--- frontend/src/app/components/admin/modules-admin/member-list-admin/member-list-admin.component.spec.ts
+++ frontend/src/app/components/admin/modules-admin/member-list-admin/member-list-admin.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MemberListAdminComponent } from './member-list-admin.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 describe('MemberListAdminComponent', () => {
   let component: MemberListAdminComponent;
@@ -8,7 +11,9 @@ describe('MemberListAdminComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MemberListAdminComponent ]
+      declarations: [ MemberListAdminComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule, MatSnackBarModule]
+
     })
     .compileComponents();
 
