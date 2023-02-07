9532166fca6542905cb66b542138faecea179ff1
TarantelFaser <jonathan@boeckel.name>
2023-02-07 20:23:38 +0100

    Changes to join module component

--- frontend/src/app/components/module/join-module/join-module.component.ts
+++ frontend/src/app/components/module/join-module/join-module.component.ts
@@ -1,44 +1,41 @@
-import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ModuleService} from "../../../../services/generated/services/module.service";
 import {ModuleDetailItem} from "../../../../services/generated/models/module-detail-item";
+import {ModuleParticipationStatus} from "../../../../services/generated/models/module-participation-status";
 
 @Component({
   selector: 'app-join-module',
   templateUrl: './join-module.component.html',
   styleUrls: ['./join-module.component.scss']
 })
-export class JoinModuleComponent implements OnInit, AfterViewInit{
+export class JoinModuleComponent implements OnInit{
   searchInput : string = "";
   @Input() infoText : string = "";
   public results: Array<ModuleDetailItem> | null = null;
-  public displayedColumns = ["moduleName", "teacher", "actions"]
+  public partStats: Array<ModuleParticipationStatus> = [];
+  public displayedColumns = ["part", "moduleName", "teacher", "actions"]
+
+  moduleParticipationStatus = ModuleParticipationStatus;
   constructor(private readonly moduleService:ModuleService) { }
 
   ngOnInit(): void {
     this.search("")
   }
 
-  search(searchString:string){
-    this.moduleService.apiModuleSearchGet$Json({
-      search:searchString
-    }).subscribe(data => {
-      this.results = data
-    })
-  }
-
-  ngAfterViewInit(): void {
-    /*
-    fromEvent<Event>(this.searchInputRef.nativeElement, "keyup")
-      .pipe(
-        debounce(v => interval(200)),
-        // @ts-ignore
-        map(v => v?.target?.value ?? "")
-      )
-      .subscribe(searchString => {
-        console.log(searchString)
-        this.search(searchString)
-      })
-     */
-  }
-
+  // search(searchString: string) {
+  //   this.moduleService.apiModuleSearchGet$Json({
+  //     search: searchString
+  //   }).subscribe(async data => {
+  //     this.results = data;
+  //     for (let module of this.results) {
+  //
+  //     }
+  //   })
+  // }
+  //
+  // private getCurrentPartStatus(module : ModuleDetailItem) {
+  //   return this.moduleService.apiModuleGetModuleParticipationStatusGet$Json({
+  //       moduleId: module.moduleId
+  //   })
+  // }
 }
