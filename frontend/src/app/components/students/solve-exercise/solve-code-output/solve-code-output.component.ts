a611c63abc1dcbfb1a75d7e91b55b939e7ce5776
TarantelFaser <jonathan@boeckel.name>
2023-02-08 18:28:59 +0100

    Initial Implementation for CodeOutput Submissions

--- frontend/src/app/components/students/solve-exercise/solve-code-output/solve-code-output.component.ts
+++ frontend/src/app/components/students/solve-exercise/solve-code-output/solve-code-output.component.ts
@@ -3,6 +3,12 @@ import {lastValueFrom} from "rxjs";
 import {CodeOutputService} from "../../../../../services/generated/services/code-output.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {CodeOutputDetailItem} from "../../../../../services/generated/models/code-output-detail-item";
+import {TimeTrackService} from "../../../../../services/generated/services/time-track.service";
+import {TimeTrackDetailItem} from "../../../../../services/generated/models/time-track-detail-item";
+import {CodeOutputSubmissionService} from "../../../../../services/generated/services/code-output-submission.service";
+import {
+  CodeOutputSubmissionDetailItem
+} from "../../../../../services/generated/models/code-output-submission-detail-item";
 
 @Component({
   selector: 'app-solve-code-output',
@@ -12,10 +18,16 @@ import {CodeOutputDetailItem} from "../../../../../services/generated/models/cod
 export class SolveCodeOutputComponent implements OnInit {
 
   @Input() id: string = "";
+  public answerString : string | null | undefined = "";
   public exercise : CodeOutputDetailItem | null = {};
+  private lastSubmission : CodeOutputSubmissionDetailItem = {};
+  private timeTrackId : string | void | undefined;
+  private timeTrack : TimeTrackDetailItem | undefined;
   public isLoading : boolean = false;
 
   constructor(private readonly codeoutputService: CodeOutputService,
+              private readonly codeoutputSubmissionService : CodeOutputSubmissionService,
+              private readonly timeTrackService: TimeTrackService,
               public snackBar: MatSnackBar,) { }
 
   ngOnInit(): void {
@@ -32,8 +44,41 @@ export class SolveCodeOutputComponent implements OnInit {
     ).then(data => {
       // @ts-ignore
       this.exercise = data;
+      this.getTimeTrack(this.exercise!.id!);
       this.isLoading = false;
     })
   }
 
+  private getTimeTrack(eId : string) {
+    console.log(eId);
+    lastValueFrom(this.timeTrackService.apiTimeTrackPost$Json( {
+      exerciseId: eId
+    })).catch(err => {
+      this.snackBar.open('Error: Could not get a TimeTrack instance!', 'dismiss');
+    }).then( data => {
+      this.timeTrackId = data;
+      this.queryLastTempSolution(eId, this.timeTrackId!);
+    })
+  }
+
+  private queryLastTempSolution(eId: string, ttId: string) {
+    lastValueFrom(this.codeoutputSubmissionService.apiCodeOutputSubmissionGetCodeOutputExerciseIdGet$Json( {
+      codeOutputExerciseId: eId,
+      currentTimeTrackId: ttId
+    })).catch( err => {
+      if (err.status != 404) {
+        this.snackBar.open('Error: Something went wrong while getting the last Submission!', 'dismiss')
+      }
+    }).then( data => {
+      this.lastSubmission = data!;
+      this.answerString = this.lastSubmission.submittedAnswer;
+    })
+  }
+
+  private createNewSubmission(ttId: string) {
+    lastValueFrom(this.codeoutputSubmissionService.apiCodeOutputSubmissionSubmitTimeTrackIdPost( {
+      timeTrackId: ttId,
+
+    }))
+  }
 }
