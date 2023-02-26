c84eae2808c09f25fa0403bf782bb0a96055c606
Jonathan <80773518+TarantelFaser@users.noreply.github.com>
2023-02-26 17:52:53 +0100

    Finished Confirming EMail Component

--- frontend/src/app/components/authentication/confirm-email/confirm-email.component.ts
+++ frontend/src/app/components/authentication/confirm-email/confirm-email.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {AuthenticateService} from "../../../../services/generated/services/authenticate.service";
+import {lastValueFrom} from "rxjs";
 @Component({
   selector: 'app-confirm-email',
   templateUrl: './confirm-email.component.html',
@@ -8,17 +9,39 @@ import {AuthenticateService} from "../../../../services/generated/services/authe
 })
 export class ConfirmEmailComponent implements OnInit {
 
+  public isLoading = false;
+  public hasSucceeded = false;
   constructor(private route: ActivatedRoute,
               private authService: AuthenticateService) { }
 
   ngOnInit(): void {
-    /*
-    this.route.params.subscribe(async params => {
-      console.log(params['token'])
-      await lastValueFrom(this.authService.CONFIRMEMAIL({
-        token: params['token']
-      }))
+    this.isLoading = true;
+    this.route.queryParams.subscribe( params => {
+      lastValueFrom(this.authService.apiAuthenticateConfirmEmailPost({
+        token: params['token'],
+        userId: params['userId']
+      })).then(() => {
+        this.hasSucceeded = true;
+      }).catch(() => {
+        this.hasSucceeded = false;
+      });
+      this.isLoading = false;
     });
-    */
   }
 }
+
+
+
+/*
+
+http://localhost:4200/confirm-email?token=CfDJ8GoDThkWwhhAmaAFMiZ2SDomUcEm1Z9q9ApNV8c7NlnmQ92wx0gQBLrPlp0B0hQeBV9PD4s7w5pjr+Ta8OzzTGiVuMR4wgrA32bGJZ6HhfDU5apn6bOIIbhVt6sARdyDrrMGyxjybX4Fm4xU4oXzoPfqLZtamFz53hehUJrkzytlLhWte2pqxEe7QqWTk8VTihdrcuqUZGvcSQqTRUwXI+r+SLS3NSf0Tc14uXBbyq7bBMNBxb7CdrsIWgESgHIQ==&userId=5b68a4fe-cde9-44a7-884f-08db180466a6
+
+http://localhost:4200/confirm-email?token=CfDJ8GoDThkWwhhAmaAFMiZ2SDomUcEm1Z9q9ApNV8c7NlnmQ92wx0gQBLrPlp0B0hQeBV9PD4s7w5pjr+Ta8OzzTGiVuMR4wgrA32bGJZ6HhfDU5apn6bOIIbhVt6sARdyDrrMGyxjybX4Fm4xU4oXzoPfqLZtamFz53hehUJrkzytlLhWte2pqxEe7QqWTk8VTihdrcuqUZGvcSQqTRUwXI+r+SLS3NSf0Tc14uXBbyq7bBMNBxb7CdrsIWgESgHIQ&userId=5b68a4fe-cde9-44a7-884f-08db180466a6
+
+http://localhost:4200/confirm-email?token=testToken&userId=testUserId
+
+http://localhost:4200/confirm-email?token=testToken
+
+http://localhost:4200/confirm-email?token=testToken&userId=testUserId
+
+ */
