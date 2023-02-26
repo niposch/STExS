387b9edfaad179a3c7869db8ee8c4299e83fb612
Jonathan <80773518+TarantelFaser@users.noreply.github.com>
2023-02-26 15:10:34 +0100

    Initial Data Dashboard Setup

--- frontend/src/app/components/admin/data-dashboard/data-dashboard.component.ts
+++ frontend/src/app/components/admin/data-dashboard/data-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ModuleService} from "../../../../services/generated/services/module.service";
+import {ActivatedRoute} from "@angular/router";
+import {lastValueFrom} from "rxjs";
+import {ModuleDetailItem} from "../../../../services/generated/models/module-detail-item";
 
 @Component({
   selector: 'app-data-dashboard',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataDashboardComponent implements OnInit {
 
-  constructor() { }
+  public moduleInfo: ModuleDetailItem | undefined;
+  public isLoading: boolean = false;
+  constructor(private readonly moduleService : ModuleService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    let moduleId : string = "";
+    this.route.queryParams.subscribe(params => {
+      moduleId = params['moduleId'];
+      this.loadModuleInfo(moduleId).then(() => {
+        this.isLoading = false;
+      })
+    })
   }
 
+  async loadModuleInfo(moduleId:string) {
+    await lastValueFrom(this.moduleService.apiModuleGetByIdGet$Json({
+      id: moduleId
+    })).then((data) => {
+      if (data == null) return;
+      this.moduleInfo = data;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 }
