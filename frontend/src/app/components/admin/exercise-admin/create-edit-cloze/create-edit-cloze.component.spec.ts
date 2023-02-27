491b0bf74b42ebad76992c061130caa0488b5bcd
niposch <niposch@gmail.com>
2023-02-27 12:30:38 +0100

    FE Tests fixed

--- frontend/src/app/components/admin/exercise-admin/create-edit-cloze/create-edit-cloze.component.spec.ts
+++ frontend/src/app/components/admin/exercise-admin/create-edit-cloze/create-edit-cloze.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateEditClozeComponent } from './create-edit-cloze.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 describe('CreateEditClozeComponent', () => {
   let component: CreateEditClozeComponent;
@@ -8,7 +11,8 @@ describe('CreateEditClozeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateEditClozeComponent ]
+      declarations: [ CreateEditClozeComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule, MatSnackBarModule]
     })
     .compileComponents();
 
