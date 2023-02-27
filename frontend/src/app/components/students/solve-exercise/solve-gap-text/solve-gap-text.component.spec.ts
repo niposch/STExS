253503ce21823d52c3fe015034ac6b5c31cfdc05
Jonathan <80773518+TarantelFaser@users.noreply.github.com>
2023-02-27 14:32:39 +0100

    Fixed Frontend Tests

--- frontend/src/app/components/students/solve-exercise/solve-gap-text/solve-gap-text.component.spec.ts
+++ frontend/src/app/components/students/solve-exercise/solve-gap-text/solve-gap-text.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SolveGapTextComponent } from './solve-gap-text.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 describe('SolveGapTextComponent', () => {
   let component: SolveGapTextComponent;
@@ -8,7 +11,8 @@ describe('SolveGapTextComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SolveGapTextComponent ]
+      declarations: [ SolveGapTextComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule, MatSnackBarModule]
     })
     .compileComponents();
 
