db85c062951a1ede38e78205f3ce6d3536c9f1c5
niposch <niposch@gmail.com>
2023-03-07 12:04:20 +0100

    Added Tests for manual Grading Method on GradingService

---
+++ backend/Application.Tests/Services/GradingServiceTests/Infrastructure.cs
@@ -0,0 +1,41 @@
+﻿using Application.Services.Grading;
+using Application.Services.Interfaces;
+using Application.Services.Permissions;
+using AutoFixture.Kernel;
+using Common.Models.Authentication;
+using Common.Models.ExerciseSystem;
+using Common.Models.ExerciseSystem.CodeOutput;
+using Common.RepositoryInterfaces.Generic;
+using Common.RepositoryInterfaces.Tables;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Repositories;
+using Repositories.Repositories.Grading;
+using TestHelper;
+
+namespace Application.Tests.Services.GradingServiceTests;
+
+public abstract class Infrastructure
+{
+    protected readonly IFixture Fixture;
+    protected readonly ApplicationDbContext Context;
+    protected readonly GradingService Service;
+    protected readonly Mock<IAccessService> AccessServiceMock;
+
+    protected Infrastructure()
+    {
+        this.Fixture = new Fixture();
+        this.Fixture.Customize(new AutoMoqCustomization());
+        this.Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        this.Context = this.Fixture.InjectInMemoryDbContext<ApplicationDbContext>();
+        this.Fixture.Register<IAccessService>(this.Fixture.Create<AccessService>);
+        this.Fixture.Register<ISubmissionRepository>(this.Fixture.Create < SubmissionRepository>);
+        this.Fixture.Register<IApplicationRepository>(this.Fixture.Create<ApplicationRepository>);
+        this.AccessServiceMock = this.Fixture.FreezeInject<IAccessService>();
+        
+        this.Fixture.Customizations.Add(new TypeRelay(typeof(BaseExercise), typeof(CodeOutputExercise)));
+
+        this.Service = this.Fixture.Create<GradingService>();
+    }
+}
---
+++ backend/Application.Tests/Services/GradingServiceTests/ManuallyGradeExerciseAsync.cs
@@ -0,0 +1,116 @@
+﻿using Common.Exceptions;
+using Common.Models.ExerciseSystem.CodeOutput;
+using Common.Models.Grading;
+using Moq;
+
+namespace Application.Tests.Services.GradingServiceTests;
+
+public sealed class ManuallyGradeExerciseAsync: Infrastructure
+{
+    [Fact]
+    public async Task ThrowsUnauthorizedAccessException_WhenUserIsNotModuleAdmin()
+    {
+        // Arrange
+        var submission = this.Fixture.Build<CodeOutputSubmission>()
+            .Without(s => s.ExerciseId)
+            .Create();
+        var newGrade = this.Fixture.Create<int>();
+        var comment = this.Fixture.Create<string>();
+        var changedByUserId = Guid.NewGuid();
+        this.Context.Submissions.Add(submission);
+        await this.Context.SaveChangesAsync();
+        this.AccessServiceMock.Setup(s => s.IsModuleAdmin(
+            It.Is<Guid>(c => c == submission.UserSubmission.Exercise.Chapter.Module.Id),
+            It.Is<Guid>(c => c == changedByUserId),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(false);
+        
+
+        // Act
+        var act = async () => await this.Service.ManuallyGradeExerciseAsync(submission.Id, newGrade, comment, changedByUserId);
+        
+        // Assert
+        await Assert.ThrowsAsync<UnauthorizedAccessException>(act);
+    }
+    
+    [Fact]
+    public async Task ThrowsEntityNotFoundException_WhenSubmissionDoesNotExist()
+    {
+        // Arrange
+        var submissionId = this.Fixture.Create<Guid>();
+        var newGrade = this.Fixture.Create<int>();
+        var comment = this.Fixture.Create<string>();
+        var changedByUserId = this.Fixture.Create<Guid>();
+
+        // Act
+        var act = async () => await this.Service.ManuallyGradeExerciseAsync(submissionId, newGrade, comment, changedByUserId);
+        
+        // Assert
+        await Assert.ThrowsAsync<EntityNotFoundException<BaseSubmission>>(act);
+    }
+    
+    [Fact]
+    public async Task CreatesGradingResult_WhenSubmissionDoesNotHaveOne()
+    {
+        // Arrange
+        var submission = this.Fixture.Build<CodeOutputSubmission>()
+            .Without(s => s.ExerciseId)
+            .Without(s => s.GradingResult)
+            .Create();
+        var newGrade = this.Fixture.Create<int>();
+        var comment = this.Fixture.Create<string>();
+        var changedByUserId = Guid.NewGuid();
+        this.Context.Submissions.Add(submission);
+        await this.Context.SaveChangesAsync();
+        this.AccessServiceMock.Setup(s => s.IsModuleAdmin(
+            It.Is<Guid>(c => c == submission.UserSubmission.Exercise.Chapter.Module.Id),
+            It.Is<Guid>(c => c == changedByUserId),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+        
+        // Act
+        await this.Service.ManuallyGradeExerciseAsync(submission.Id, newGrade, comment, changedByUserId);
+        
+        // Assert
+        var result = this.Context.Submissions.Find(submission.Id);
+        result.GradingResult.Should().NotBeNull();
+        result.GradingResult.GradingState.Should().Be(GradingState.FinallyManuallyGraded);
+        result.GradingResult.Points.Should().Be(newGrade);
+        result.GradingResult.Comment.Should().Be(comment);
+        result.GradingResult.ManualGradingDate.Should().BeCloseTo(DateTime.Now, new TimeSpan(0, 1, 0));
+        result.GradingResult.IsAutomaticallyGraded.Should().BeFalse();
+        result.GradingResult.AppealableBefore.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task ShouldUpdateExistingManualGradingResult()
+    {
+        // Arrange
+        var submission = this.Fixture.Build<CodeOutputSubmission>()
+            .Without(s => s.ExerciseId)
+            .Create();
+        var newGrade = this.Fixture.Create<int>();
+        var comment = this.Fixture.Create<string>();
+        var changedByUserId = Guid.NewGuid();
+        this.Context.Submissions.Add(submission);
+        await this.Context.SaveChangesAsync();
+        this.AccessServiceMock.Setup(s => s.IsModuleAdmin(
+            It.Is<Guid>(c => c == submission.UserSubmission.Exercise.Chapter.Module.Id),
+            It.Is<Guid>(c => c == changedByUserId),
+            It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+        
+        // Act
+        await this.Service.ManuallyGradeExerciseAsync(submission.Id, newGrade, comment, changedByUserId);
+        
+        // Assert
+        var result = this.Context.Submissions.Find(submission.Id);
+        result.GradingResult.Should().NotBeNull();
+        result.GradingResult.GradingState.Should().Be(GradingState.FinallyManuallyGraded);
+        result.GradingResult.Points.Should().Be(newGrade);
+        result.GradingResult.Comment.Should().Be(comment);
+        result.GradingResult.ManualGradingDate.Should().BeCloseTo(DateTime.Now, new TimeSpan(0, 1, 0));
+        result.GradingResult.IsAutomaticallyGraded.Should().BeFalse();
+        result.GradingResult.AppealableBefore.Should().BeNull();
+    }
+}
