d12bf38536a532233610961e27d3eeceae47a7ba
niposch <niposch@gmail.com>
2023-03-08 16:24:24 +0100

    fixed fe unit tests

--- frontend/src/app/components/students/solve-exercise/solve-gap-text/solve-gap-text.component.ts
+++ frontend/src/app/components/students/solve-exercise/solve-gap-text/solve-gap-text.component.ts
@@ -1,49 +1,51 @@
 import {
-  OnInit, AfterViewInit,ChangeDetectorRef,
-  Component, EventEmitter,
-  Input, OnDestroy, Output,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
   ViewChild,
 } from '@angular/core';
-import {lastValueFrom} from "rxjs";
-import {ClozeTextExerciseDetailItem} from "../../../../../services/generated/models/cloze-text-exercise-detail-item";
-import {ClozeTextExerciseService} from "../../../../../services/generated/services/cloze-text-exercise.service";
-import {TimeTrackService} from "../../../../../services/generated/services/time-track.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {ViewClozeComponent} from 'src/app/components/admin/exercise-admin/create-edit-cloze/view-cloze/view-cloze.component';
-import {ClozeTextSubmissionService} from '../../../../../services/generated/services/cloze-text-submission.service';
-import {ClozeTextSubmissionDetailItem} from '../../../../../services/generated/models/cloze-text-submission-detail-item';
-
-
+import { lastValueFrom } from 'rxjs';
+import { ClozeTextExerciseDetailItem } from '../../../../../services/generated/models/cloze-text-exercise-detail-item';
+import { ClozeTextExerciseService } from '../../../../../services/generated/services/cloze-text-exercise.service';
+import { TimeTrackService } from '../../../../../services/generated/services/time-track.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ViewClozeComponent } from 'src/app/components/admin/exercise-admin/create-edit-cloze/view-cloze/view-cloze.component';
+import { ClozeTextSubmissionService } from '../../../../../services/generated/services/cloze-text-submission.service';
+import { ClozeTextSubmissionDetailItem } from '../../../../../services/generated/models/cloze-text-submission-detail-item';
 
 @Component({
   selector: 'app-solve-gap-text',
   templateUrl: './solve-gap-text.component.html',
-  styleUrls: ['./solve-gap-text.component.scss']
+  styleUrls: ['./solve-gap-text.component.scss'],
 })
 export class SolveGapTextComponent implements OnInit, OnDestroy {
-
-  @Input() id: string = "";
-  public text: string = "";
+  @Input() id: string = '';
+  public text: string = '';
   public answerStrings: string[] | undefined | null;
 
   private lastSubmission: ClozeTextSubmissionDetailItem | undefined;
   public timeTrackId: string | null | undefined = null;
   public exercise: ClozeTextExerciseDetailItem | null = {};
-  public isLoading : boolean = false;
+  public isLoading: boolean = false;
   public isSubmitting: boolean = false;
-  @Output() solvedChange : EventEmitter<any> = new EventEmitter<any>();
+  @Output() solvedChange: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(
-    private readonly clozeTextService : ClozeTextExerciseService,
+    private readonly clozeTextService: ClozeTextExerciseService,
     private readonly clozetextSubmissionService: ClozeTextSubmissionService,
     private readonly timeTrackService: TimeTrackService,
     public snackBar: MatSnackBar,
-    private readonly changeDetectorRef: ChangeDetectorRef) { }
-
+    private readonly changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   //@ViewChild(ViewClozeComponent) child!: ViewClozeComponent;
 
-  ngOnInit():void {
+  ngOnInit(): void {
     this.isLoading = true;
     this.changeDetectorRef.detectChanges();
     void this.loadExercise();
@@ -129,7 +131,7 @@ export class SolveGapTextComponent implements OnInit, OnDestroy {
     ttId: string | null | undefined,
     isFinal: boolean = false,
     answer: string[] | undefined | null = null
-  ) : Promise<any> {
+  ): Promise<any> {
     try {
       this.isSubmitting = true;
       await lastValueFrom(
@@ -143,9 +145,12 @@ export class SolveGapTextComponent implements OnInit, OnDestroy {
             },
           }
         )
-      )
+      );
       if (isFinal) {
-        this.solvedChange.emit({solved: true, exerciseId: this.exercise!.id!});
+        this.solvedChange.emit({
+          solved: true,
+          exerciseId: this.exercise!.id!,
+        });
         this.exercise!.userHasSolvedExercise = true;
       }
       this.isSubmitting = false;
@@ -156,10 +161,11 @@ export class SolveGapTextComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     if (this.exercise?.userHasSolvedExercise) return;
+    if (this.answerStrings == null) return;
     this.tempSave(this.answerStrings!).then(() => {
       if (this.timeTrackId == null) return;
       this.closeTimeTrack(this.timeTrackId);
-    })
+    });
   }
 
   private closeTimeTrack(ttId: string) {
@@ -170,11 +176,10 @@ export class SolveGapTextComponent implements OnInit, OnDestroy {
     );
   }
 
-  async tempSave(answerString: Array <string>) : Promise<any> {
+  async tempSave(answerString: Array<string>): Promise<any> {
     if (answerString == undefined) return;
     if (this.exercise?.userHasSolvedExercise) return;
     console.log('tempSave', answerString);
     await this.createNewSubmission(this.timeTrackId, false, answerString);
   }
-
 }
